b5b714147f513384e75e9859c6881f1c7d9b1c6e
mehdi <mmouslih30@gmail.com>
2023-12-06 10:30:19 -0800

    ffkf

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(" {} + {} = {}".format(a, b, add(a, b)))
-    print(" {} - {} = {}".format(a, b, sub(a, b)))
-    print(" {} * {} = {}".format(a, b, mul(a, b)))
-    print(" {} / {} = {}".format(a, b, div(a, b)))
+
+    resadd = add(a, b)
+    ressub = sub(a, b)
+    resmul = mul(a, b)
+    resdiv = div(a, b)
+
+    print(f"{a} + {b} = {resadd}")
+    print(f"{a} - {b} = {ressub}")
+    print(f"{a} * {b} = {resmul}")
+    print(f"{a} / {b} = {resdiv}")
