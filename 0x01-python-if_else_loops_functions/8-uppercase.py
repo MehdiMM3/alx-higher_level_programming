376480d77cc7018da13699d3917a03d2324da6e5
mehdi <mmouslih30@gmail.com>
2023-12-12 04:09:05 -0800

    joednr

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return True
-    else:
-        return False
-
 def uppercase(pk):
     for char in pk:
         upper_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
