7b1b52c2d0fa1ae848b809cc0983683639acc3da
mehdi <mmouslih30@gmail.com>
2023-12-12 03:37:46 -0800

    rjrjz

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print(chr(i), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('q') and i != ord('e'):
+        print("{:c}".format(i), end="")
