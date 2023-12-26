cfe0ce0f2c82ec01822fa9b7b760c30b8469b749
mehdi <mmouslih30@gmail.com>
2023-12-26 01:36:08 -0800

    k

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -29,3 +29,34 @@ class Node:
         def next_node(self):
             """Get/set the next_node of the Node."""
             return(self.__next_node)
+
+        @next_node.setter
+        def next_node(self, value):
+            if not isinstance(value, node) and value is not None:
+                raise TypeError("next_node must be a Node object")
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Represent a singly-linked list."""
+
+    def __init__(self):
+        """Initialize a new SinglyLinkedList."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new node to the SignlyLinkedList.
+        The Node is inserted into the list at the correct
+        ordered numerical position.
+        Args:
+            value (Node): The new Node to insert.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
