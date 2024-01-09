7527c4737881e4a1325e54495d0787adf9c13276
mehdi <mmouslih30@gmail.com>
2024-01-09 07:17:08 -0800

    kcu

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """append a file with utf-8"""
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
 
