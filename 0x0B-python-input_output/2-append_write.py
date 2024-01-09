ae427ad59988fa1bdf8a2312e59209f95af667a1
mehdi <mmouslih30@gmail.com>
2024-01-09 07:14:15 -0800

    kdeo

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    """append a file with utf-8"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
+
