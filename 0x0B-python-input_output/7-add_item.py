4efae2b013f1302ca400d96b84338c97c3cae16c
mehdi <mmouslih30@gmail.com>
2024-01-09 08:30:34 -0800

    h

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,16 @@
 """task7"""
 import sys
 
-if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
+        arglist = list(sys.argv[1:])
+
     try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        esdf = load_from_json_file("add_item.json")
+    except Exception:
+        esdf = []
+    esdf.extend(arglist)
+    save_to_json_file(esdf, "add_item.json")
 
